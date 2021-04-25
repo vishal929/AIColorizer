98c12c90bee5b78e6d406b106946bf0d1e40158d
vishal929 <48373882+vishal929@users.noreply.github.com>
2021-04-25 18:19:46 -0400

    made some speed improvements in clustering

--- colorize.py
+++ colorize.py
@@ -30,22 +30,28 @@ def bwImage(colorImage):
     # returns the numpy array representing the recolored left half of the image
 def kmeans(colorImage,numMeans):
     ogWidth,ogLength,ogDim= numpy.shape(colorImage)
+    print("width, length, and dim of colored image: " + str(ogWidth) + ", " + str(ogLength) + ", " + str(ogDim))
     colorLeftHalf = colorImage[:,:int(ogLength/2),:]
     width,length,dim = numpy.shape(colorLeftHalf)
+    print("width, length, and dim of cropped left half of colored image: "+str(width)+", "+str(length)+", "+str(dim))
     meansChosen = 0
-    means = numpy.array([])
+    means=[]
+    #means = numpy.array([])
     while (meansChosen != numMeans):
         randRow = randint(0, width-1)
         randCol = randint(0, length-1)
-        meanChosen = colorLeftHalf[randRow, randCol, 0], \
+        meanChosen = (colorLeftHalf[randRow, randCol, 0], \
                      colorLeftHalf[randRow, randCol, 1], \
-                     colorLeftHalf[randRow, randCol, 2]
-        if (meanChosen in means):
+                     colorLeftHalf[randRow, randCol, 2])
+        if ((meanChosen,[]) in means):
             # then we have a repeat value, which is bad
             continue
         # empty list is for later mapping of pixels
-        numpy.append(means, (meanChosen, numpy.array([])))
+        means.append((meanChosen,{}))
+        #means.append((meanChosen,[]))
+        #numpy.append(means, (meanChosen, numpy.array([])))
         meansChosen += 1
+    print("CHOSE INITIAL "+str(meansChosen)+ " MEANS!")
     # doing initial mapping of means
     for i in range(width):
         for j in range(length):
@@ -55,23 +61,47 @@ def kmeans(colorImage,numMeans):
             closestMeanCost = None
             for z in range(len(means)):
                 # by "closest" we take (r_1-r_2)^2 + (g_1-g_2)^2 + (b_1-b_2)^2 and minimize this
-                cost = (colorLeftHalf[i, j, 0] - means[z][0]) ^ 2 + \
-                       (colorLeftHalf[i, j, 1] - means[z][1]) ^ 2 + \
-                       (colorLeftHalf[i, j, 2] - means[z][2]) ^ 2
+                #print(colorLeftHalf[i,j])
+                #print(means[z][0])
+                cost = ((int(colorLeftHalf[i, j, 0]) - int(means[z][0][0])) ** 2) + \
+                       ((int(colorLeftHalf[i, j, 1]) - int(means[z][0][1])) ** 2) + \
+                       ((int(colorLeftHalf[i, j, 2]) - int(means[z][0][2])) ** 2)
                 if ((closestMean is None) or (cost < closestMeanCost)):
                     closestMean = z
                     closestMeanCost = cost
             # now we know the closest mean index
-            numpy.append(means[closestMean][1],(colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2]))
+            colorToAdd = (colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2])
+            if (colorToAdd in means[closestMean][1]):
+                means[closestMean][1][colorToAdd] +=1
+            else:
+                means[closestMean][1][colorToAdd] =1
+            #means[closestMean][1].append((colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2]))
+
+            #numpy.append(means[closestMean][1],(colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2]))
+    print("FINISHED INITIAL MAPPING")
 
     # running actual k means algorithm now
     # converting to numpy array
     oldMeans = means
     while True:
-        means = numpy.array([])
+        #means = numpy.array([])
+        means=[]
         # firstly getting new means
         for i in range(len(oldMeans)):
-            numpy.append(means, (numpy.average(oldMeans[i][1]), numpy.array([])))
+            R=0
+            G=0
+            B=0
+            totalValues=0
+            for value in oldMeans[i][1]:
+                numOccurences = oldMeans[i][1][value]
+                totalValues += numOccurences
+                R += value[0]*numOccurences
+                G += value[1]*numOccurences
+                B += value[2]*numOccurences
+            means.append(((int(R/totalValues),int(G/totalValues),int(B/totalValues)), {}))
+            #means.append((sum(i) for i in zip(*oldMeans[i][1]))/len(oldMeans[i][1]) , [])
+            #means.append( (sum(oldMeans[i][1])/len(oldMeans[i][1]), []))
+            #numpy.append(means, (numpy.average(oldMeans[i][1]), numpy.array([])))
         # adding new neighbors to the respective lists of associated pixels
         for i in range(numpy.shape(colorLeftHalf)[0]):
             for j in range(numpy.shape(colorLeftHalf)[1]):
@@ -81,31 +111,36 @@ def kmeans(colorImage,numMeans):
                 closestMeanCost = None
                 for z in range(len(means)):
                     # by "closest" we take (r_1-r_2)^2 + (g_1-g_2)^2 + (b_1-b_2)^2 and minimize this
-                    cost = (colorLeftHalf[i, j, 0] - means[z][0]) ^ 2 + \
-                           (colorLeftHalf[i, j, 1] - means[z][1]) ^ 2 + \
-                           (colorLeftHalf[i, j, 2] - means[z][2]) ^ 2
+                    cost = (int(colorLeftHalf[i, j, 0]) - int(means[z][0][0])) ** 2 + \
+                           (int(colorLeftHalf[i, j, 1]) - int(means[z][0][1])) ** 2 + \
+                           (int(colorLeftHalf[i, j, 2]) - int(means[z][0][2])) ** 2
                     if ((closestMean is None) or (cost < closestMeanCost)):
                         closestMean = z
                         closestMeanCost = cost
                 # now we know the closest mean index
-                numpy.append(means[closestMean][1],
-                             (colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2]))
+                colorToAdd = (colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2])
+                if (colorToAdd in means[closestMean][1]):
+                    means[closestMean][1][colorToAdd] += 1
+                else:
+                    means[closestMean][1][colorToAdd] = 1
+                #numpy.append(means[closestMean][1],
+                             #(colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2]))
+                #means[closestMean][1].append((colorLeftHalf[i, j, 0], colorLeftHalf[i, j, 1], colorLeftHalf[i, j, 2]))
         # checking if any changes are made to assignments between means and oldmeans
         changes = False
         for i in range(len(means)):
-            if set(means[i][1]) != set(oldMeans[i][1]):
-                changes = True
+            for key in means[i][1]:
+                if key not in oldMeans[i][1] or means[i][1][key] != oldMeans[i][1][key]:
+                    changes = True
+                    break
+            if changes:
                 break
-
         if not changes:
             # then we hit convergence
-            for i in range(len(means)):
-                # converting to set, so we can easily see the pixels to recolor
-                means[i][1] = set(means[i][1])
             break
         else:
             oldMeans = means
-
+    print("REACHED CONVERGENCE! RECOLORING IMAGE NOW")
     '''
     Recolor left half of image
     '''
@@ -122,6 +157,7 @@ def kmeans(colorImage,numMeans):
                     break
     # now the left half of the color image is "recolored"
     # returning the recolored left half of the image
+    print("FINISHED RECOLORING IMAGE")
     return colorLeftHalf
 
 
@@ -130,10 +166,16 @@ def kmeans(colorImage,numMeans):
 # colorImage is a numpy3d array of pixels representing the recolored left half of the color image
 # blackWhite image is the entire black and white image as a 2d numpy array of grey values
 def knn(colorImage, blackWhiteImage):
+    print("STARTING KNN")
     width,length = numpy.shape(blackWhiteImage)
+    print("width, length of bw image: "+str(width)+", "+str(length))
     # getting left hand side of image and right hand side of image
     blackWhiteTraining = blackWhiteImage[:,:int(length/2)]
+    trainingWidth,trainingLength = numpy.shape(blackWhiteTraining)
+    print("width, length of bw training: " + str(trainingWidth) + ", " + str(trainingLength))
     blackWhiteTest = blackWhiteImage[:,:(length-int(length/2))]
+    testWidth,testLength = numpy.shape(blackWhiteTest)
+    print("width, length of bw test: " + str(testWidth) + ", " + str(testLength))
     # need to go through all 3x3 patches on left hand side of image and associate them with a value
         # and representative color
     trainingShape = numpy.shape(blackWhiteTraining)
@@ -176,7 +218,7 @@ def knn(colorImage, blackWhiteImage):
             # if we reached here, we have a valid 3x3 patch
             # now we need to go to the training data and find the 6 closest 3x3 patches
                 # form of this is [(distanceValue, (rgb)) , ...]
-            sixClosest = numpy.array([])
+            sixClosest = []
             for z in range(trainingShape[0]):
                 # need to see if surrounding squares are valid
                 # if not, then this cannot be a middle square for a 3x3 patch
@@ -209,14 +251,19 @@ def knn(colorImage, blackWhiteImage):
                     # seeing if we can add this data to the list
                     if (len(sixClosest)<6):
                         # then we can just add it
-                        numpy.append(sixClosest,(distance,rgb))
+                        #numpy.append(sixClosest,(distance,rgb))
+                        sixClosest.append((distance,rgb))
                     else:
                         # then we have to replace this with the greatest value if it is larger
-                        largest = numpy.argmax(sixClosest)
-                        if sixClosest[largest][0]>distance:
+                        largest=None
+                        for closest in sixClosest:
+                            if largest is None or closest[0]>largest[0]:
+                                largest=closest
+                        #largest = numpy.argmax(sixClosest)
+                        if largest[0]>distance:
                             # then we can replace it
-                            numpy.delete(sixClosest,largest)
-                            numpy.append(sixClosest,(distance,rgb))
+                            sixClosest.remove(largest)
+                            sixClosest.append((distance,rgb))
             # now we have the six closest neighbors of this patch
             # if there is a win in representative colors, we pick that color
                 # otherwise pick color with least distance
@@ -266,6 +313,9 @@ def improved(image,model):
 
 #test
 colorImage = imageToArray("colorImage.jfif")
+# making image much smaller for calculation purposes (og image has 1,105,440 pixels to process)
+colorWidth,colorLength,colorDim = numpy.shape(colorImage)
+colorImage =colorImage[:int(colorWidth/2),:int(colorLength/2),:]
 blackWhiteArray = bwImage(colorImage)
 
 recoloredLeftHalf = kmeans(colorImage,5)
