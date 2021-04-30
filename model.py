cc1354caedf90ecb556c9c67efe225684290351d
vishal929 <48373882+vishal929@users.noreply.github.com>
2021-04-30 18:11:17 -0400

    added skeleton for improved model

---
+++ model.py
@@ -0,0 +1,24 @@
+# just a file to organize our improved model thoughts
+
+def evaluateEquation():
+    pass
+
+def evaluateDerivative():
+    pass
+
+def stochasticGradientDescent(equationFunction,derivativeFunction):
+    pass
+
+def trainModel(blackWhiteTraining,colorTraining):
+    pass
+
+#generates a color image from blackWhiteTest and returns the observed loss when compared to colorTest
+def testModel(blackWhiteTest,colorTest):
+    pass
+
+def loadWeightsFromFile(fileName):
+    pass
+
+def writeWeightsToFile(filename):
+    pass
+
